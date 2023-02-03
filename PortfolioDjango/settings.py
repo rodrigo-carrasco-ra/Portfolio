16f8fc4f5b816ef545d9bda1d8167f4856444fe1
Rodrigo Carrasco <rodrigo.carrasco40@inacapmail.cl>
2023-02-02 23:42:56 -0300

    arreglado allowed host2

--- PortfolioDjango/settings.py
+++ PortfolioDjango/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',  
             'NAME': 'portfolio',  
             'USER': 'root',  
-            'PASSWORD': '44femx3G!',  
+            'PASSWORD': '',  
             'HOST': '127.0.0.1',  
             'PORT': '3306',  
             'OPTIONS': {  
